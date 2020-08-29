cc0b892bc4a1e22875b58c3710f3d2424017daab
walax31 <walax31009@gmail.com>
2020-08-29 19:52:40 +0700

    add subject controller page

---
+++ app/Controllers/Http/SubjectController.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const Database = use(`Database`);
+function numberTypeParameValidator(number) {
+  if (Number.isNaN(parseInt(number))) {
+    error: `param:${number} is not supported,please use number type param intnstead`;
+  }
+  return {};
+}
+
+class SubjectController {
+    async index() {
+        const subject = await Database.table("subjects");
+    
+        return { status: 200, error: undefined, data: group };
+      }
+      async show({ request }) {
+        const { id } = request.params;
+    
+        const validateValue = numberTypeParameValidator(id);
+    
+        if (validateValue.error)
+          return { status: 500, error: validateValue.error, data: undefined };
+    
+        const enrollments = await Database.select("*")
+          .from("subjects")
+          .where("subject_id", id)
+          .first();
+    
+        return { status: 200, error: undefined, data: subject || {} };
+      }
+      async store({ request }) {
+        const { title} = request.body;
+        const missingKeys = [];
+        if (!title) missingKeys.push("title");
+        if (missingKeys.length)
+          return {
+            status: 422,
+            error: `${missingKeys} is missing.`,
+            data: undefined,
+          };
+        const enrollment = await Database.table("subjects").insert({ title });
+    
+        return { status: 200, error: undefined, data: { title } };
+      }
+}
+
+module.exports = SubjectController
