1d8f5a0cccc643551767991af066a17d71b58119
walax31 <walax31009@gmail.com>
2020-08-29 19:50:13 +0700

    add page enrollment controller

---
+++ app/Controllers/Http/EnrollmentController.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const Database = use(`Database`);
+
+function numberTypeParameValidator(number) {
+  if (Number.isNaN(parseInt(number))) {
+    error: `param:${number} is not supported,please use number type param intnstead`;
+  }
+  return {};
+}
+
+class EnrollmentController {
+  async index() {
+    const enrollments = await Database.table("enrollments");
+
+    return { status: 200, error: undefined, data: enrollments };
+  }
+  async show({ request }) {
+    const { id } = request.params;
+
+    const validateValue = numberTypeParameValidator(id);
+
+    if (validateValue.error)
+      return { status: 500, error: validateValue.error, data: undefined };
+
+    const enrollments = await Database
+      .select('*')
+      .from('enrollments')
+      .where("enrollments_id", id)
+      .first()
+
+    return { status: 200, error: undefined, data: enrollments || {} };
+  }
+  async store({ request }) {
+
+    const { mark } = request.body
+    const missingKeys = []
+
+    if (!mark) missingKeys.push('make')
+    if (missingKeys.length)
+      return {
+        status: 422,
+        error: `${missingKeys} is missing.`,
+        data: undefined,
+      }
+      const enrollment =await Database
+      .table('enrollments')
+      .insert({mark})
+
+      return{status:200,error: undefined,data:{mark}}
+  }
+}
+
+module.exports = EnrollmentController;
