f3681f69d96e28b426a3600cad0414e4189e2bed
walax31 <walax31009@gmail.com>
2020-08-29 18:04:33 +0700

    add studentcontroller

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,7 +1,56 @@
 'use strict'
 
-class StudentController {
-    
+const Database= use('Database')
+const Hash = use('Hash')
+
+function numberTypeParameValidator(number){
+    if (Number.isNaN(parseInt(number))){error: `param:${number} is not supported,please use number type param intnstead`}
+    return {}
 }
+class StudentController {
+    async index(){
+        const students = await Database.table('students')
+
+        return{ status:200,error: undefined,data:students}
+    }
+    async show({request}){
+        const {id} =request.params
+
+        const validateValue =numberTypeParameValidator(id)
+
+        if (validateValue.error)
+        return {status:500 ,error:validateValue.error,data: undefined}
+    
+    const students= await Database
+    .select('*')
+    .from('students')
+    .where("student_id", id)
+    .first()
+
+    return { status: 200, error: undefined, data: students || {} }
+  }
+  async store({request}){
+      const { first_name, last_name, email, password } = request.body
+      const missingKeys = []
+      if (!first_name) missingKeys.push('first_name')
+    if (!last_name) missingKeys.push('last_name')
+    if (!email) missingKeys.push('email')
+    if (!password) missingKeys.push('password')
+
+    if (missingKeys.length)
+      return { status: 422, error: `${missingKeys} is missing.`, data: undefined }
+
+    const hashedPassword = await Hash.make(password)
+
+
+    const student = await Database
+      .table('students')
+      .insert({ first_name, last_name, email, password: hashedPassword })
+
+    return { status: 200, error: undefined, data: { first_name, last_name, email } }
+  }
+  }
+
+
 
 module.exports = StudentController
