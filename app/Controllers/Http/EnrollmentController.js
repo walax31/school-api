c8512df75467d126ae39abd4ea1a2b3b267fad75
walax31 <walax31009@gmail.com>
2020-09-01 13:17:55 +0700

    edit enrollment controller

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -1,13 +1,16 @@
 "use strict";
 
 const Database = use(`Database`);
+const Validator = use("Validator");
 
-
-  function numberTypeParamValidator(number) {
-    if (Number.isNaN(parseInt(number))) { return { error: `param:${number} is not supported,please use number type param intnstead` } }
-    return {}
+function numberTypeParamValidator(number) {
+  if (Number.isNaN(parseInt(number))) {
+    return {
+      error: `param:${number} is not supported,please use number type param intnstead`,
+    };
   }
-
+  return {};
+}
 
 class EnrollmentController {
   async index() {
@@ -18,36 +21,57 @@ class EnrollmentController {
   async show({ request }) {
     const { id } = request.params;
 
-    const validateValue = numberTypeParamValidator(id)
+    const validateValue = numberTypeParamValidator(id);
 
     if (validateValue.error)
-    return { status: 500, error: validateValue.error, data: undefined }
+      return { status: 500, error: validateValue.error, data: undefined };
 
-    const enrollments = await Database
-      .select('*')
-      .from('enrollments')
+    const enrollments = await Database.select("*")
+      .from("enrollments")
       .where("enrollments_id", id)
-      .first()
+      .first();
 
     return { status: 200, error: undefined, data: enrollments || {} };
   }
   async store({ request }) {
+    const { mark } = request.body;
+    const rules = {
+      first_name: "required",
+      last_name: "requried",
+      email: "required|email|unique:teachers,email",
+      password: "required|min:8",
+    };
 
-    const { mark } = request.body
-    const missingKeys = []
+    const validation = await Validator.validateAll(request.body, rules);
+    if (validation.fails())
+      return { status: 422, error: validation.messages(), data: undefined };
 
-    if (!mark) missingKeys.push('make')
-    if (missingKeys.length)
-      return {
-        status: 422,
-        error: `${missingKeys} is missing.`,
-        data: undefined,
-      }
-      const enrollment =await Database
-      .table('enrollments')
-      .insert({mark})
+    const enrollment = await Database.table("enrollments").insert({ mark });
 
-      return{status:200,error: undefined,data:{mark}}
+    return { status: 200, error: undefined, data: { mark } };
+  }
+
+  async update({ request }) {
+    const { body, params } = request;
+    const { id } = params;
+    const { mark } = body;
+
+    const enrollmentId = await Database.table("enrollments")
+      .where({ enrollments_id: id })
+      .update({ mark });
+
+    const enrollment = await Database.table("enrollments")
+      .where({ enrollments_id: enrollmentId })
+      .first();
+
+    return { status: 200, error: undefined, data: { massage: "success" } };
+  }
+  async destroy({ request }) {
+    const { id } = request.params;
+
+    await Database.table("enrollments").where({ enrollments_id: id }).delete();
+
+    return { status: 200, error: undefined, data: { massage: "success" } };
   }
 }
 
