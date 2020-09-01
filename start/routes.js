e0cbfc5e87aeda550b1fbcd0659021b6a3ac23e2
walax31 <walax31009@gmail.com>
2020-09-01 13:13:37 +0700

    edit routes

--- start/routes.js
+++ start/routes.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const TeacherController = require('../app/Controllers/Http/TeacherController')
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,24 +24,41 @@ Route.get('/', () => {
 
 Route.group(()=>{
   //api rount start here na ka
-  Route.get('/teachers','TeacherController.index')
-  Route.get('/teachers/:id','TeacherController.show')
-  Route.post('/teachers','TeacherController.store')
+  Route.resource('/teachers','TeacherController')
 
-  Route.get('/students','StudentController.index')
-  Route.get('/students/:id','StudentController.show')
-  Route.post('/students','StudentController.store')
+  Route.resource('/subjects','SubjectController')
 
-  Route.get('/enrollments','EnrollmentController.index')
-  Route.get('/enrollments/:id','EnrollmentController.show')
-  Route.post('/enrollments','EnrollmentController.store')
+  Route.resource('/students','StudentController')
+
+  Route.resource('/groups','GroupController')
+
+  Route.resource('/enrollments','EnrollmentController')
+
+
+  // Route.get('/teachers','TeacherController.index')
+  // Route.get('/teachers/:id','TeacherController.show')
+  // Route.post('/teachers','TeacherController.store')
+  // Route.put('/teachers/:id','TeacherController.update')
+  // Route.patch('/teachers/:id','TeacherController.update')
+  // Route.delete('/teachers/:id','TeacherController.destroy')
+
+
+  // Route.get('/students','StudentController.index')
+  // Route.get('/students/:id','StudentController.show')
+  // Route.post('/students','StudentController.store')
+
+  // Route.get('/enrollments','EnrollmentController.index')
+  // Route.get('/enrollments/:id','EnrollmentController.show')
+  // Route.post('/enrollments','EnrollmentController.store')
 
   
-  Route.get('/groups','GroupController.index')
-  Route.get('/groups/:id','GroupController.show')
-  Route.post('/groups','GroupController.store')
+  // Route.get('/groups','GroupController.index')
+  // Route.get('/groups/:id','GroupController.show')
+  // Route.post('/groups','GroupController.store')
 
-  Route.get('/subjects','SubjectController.index')
-  Route.get('/subjects/:id','SubjectController.show')
-  Route.post('/subjects','SubjectController.store')
+  // Route.get('/subjects','SubjectController.index')
+  // Route.get('/subjects/:id','SubjectController.show')
+  // Route.post('/subjects','SubjectController.store')
+
+ 
 }).prefix('api/v1')
