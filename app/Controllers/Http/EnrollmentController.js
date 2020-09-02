c4e9c4dd5d24f84a24fe18ad8f01e477f52fdab4
walax31 <walax31009@gmail.com>
2020-09-02 19:44:16 +0700

    edit enrollment controller

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -2,6 +2,7 @@
 
 const Database = use(`Database`);
 const Validator = use("Validator");
+const Enrollment= use('App/Models/Enrollment')
 
 function numberTypeParamValidator(number) {
   if (Number.isNaN(parseInt(number))) {
@@ -13,44 +14,59 @@ function numberTypeParamValidator(number) {
 }
 
 class EnrollmentController {
-  async index() {
-    const enrollments = await Database.table("enrollments");
+  async index({request}) {
+    const {references=undefined} = request.qs
+    const enrollments=Enrollment.query()
+    if (references){
+      const extractedReferences =references.split(",")
+      for(const value of extractedReferences){
+        enrollments.with(value)
+      }
+      
+    } 
 
-    return { status: 200, error: undefined, data: enrollments };
+    return { status: 200, error: undefined, data:await enrollments.fetch() };
   }
+  
+  
+  
+  
+  
   async show({ request }) {
     const { id } = request.params;
-
+    const enrollment =await Enrollment.find(id)
     const validateValue = numberTypeParamValidator(id);
 
     if (validateValue.error)
       return { status: 500, error: validateValue.error, data: undefined };
 
-    const enrollments = await Database.select("*")
-      .from("enrollments")
-      .where("enrollments_id", id)
-      .first();
+    
 
     return { status: 200, error: undefined, data: enrollments || {} };
   }
+
+
+
   async store({ request }) {
-    const { mark } = request.body;
+    const { mark,student_id,subject_id } = request.body;
+   
     const rules = {
-      first_name: "required",
-      last_name: "requried",
-      email: "required|email|unique:teachers,email",
-      password: "required|min:8",
+     mark:"required"
     };
 
     const validation = await Validator.validateAll(request.body, rules);
     if (validation.fails())
       return { status: 422, error: validation.messages(), data: undefined };
-
-    const enrollment = await Database.table("enrollments").insert({ mark });
-
-    return { status: 200, error: undefined, data: { mark } };
+      const enrollment = new Enrollment()
+      enrollment.mark =mark
+      enrollment.student_id=student_id
+      enrollment.subject_id=subject_id
+  
+      await enrollment.save()
+      return { status: 200, error: undefined, data: { mark } };
   }
 
+  
   async update({ request }) {
     const { body, params } = request;
     const { id } = params;
