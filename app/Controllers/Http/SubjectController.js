22291f9558f96e00a386c3309bd4b0f9fa0fa5d8
walax31 <walax31009@gmail.com>
2020-09-03 20:08:02 +0700

    edit format

--- app/Controllers/Http/SubjectController.js
+++ app/Controllers/Http/SubjectController.js
@@ -2,7 +2,7 @@
 
 const Database = use(`Database`);
 const Validator = use("Validator");
-const Subject =use('App/Models/Subject')
+const Subject = use("App/Models/Subject");
 
 function numberTypeParamValidator(number) {
   if (Number.isNaN(parseInt(number))) {
@@ -14,64 +14,48 @@ function numberTypeParamValidator(number) {
 }
 
 class SubjectController {
-  async index({request}) {
+  async index({ request }) {
     //? /subjects?references
-    const { references=undefined} =request.qs
-    const subjects = Subject.query()
-    if (references){
-       const extractedReferences =references.split(",")
-       subjects.with(extractedReferences)
+    const { references = undefined } = request.qs;
+    const subjects = Subject.query();
+    if (references) {
+      const extractedReferences = references.split(",");
+      subjects.with(extractedReferences);
     }
-     return { status: 200, error: undefined, data:await subjects.fetch() };
+    return { status: 200, error: undefined, data: await subjects.fetch() };
   }
- 
- 
+
   async show({ request }) {
     const { id } = request.params;
-    const subject =await Subject.find(id)
+    const subject = await Subject.find(id);
     const validateValue = numberTypeParamValidator(id);
 
     if (validateValue.error)
       return { status: 500, error: validateValue.error, data: undefined };
 
-
-
     return { status: 200, error: undefined, data: subject || {} };
   }
-  
-  
- 
- 
- 
- 
- 
+
   async store({ request }) {
-    const { title,teacher_id } = request.body;
-    
-   
+    const { title, teacher_id } = request.body;
+
     const rules = {
-      title: "required"
-    }
+      title: "required",
+    };
 
     const validation = await Validator.validateAll(request.body, rules);
     if (validation.fails())
       return { status: 422, error: validation.messages(), data: undefined };
-    
-    const subject = new Subject()
-    subject.title =title
-    subject.teacher_id =teacher_id
-  
-    await subject.save()
-    
 
-    return {status:200 ,error:undefined ,data: title}
+    const subject = new Subject();
+    subject.title = title;
+    subject.teacher_id = teacher_id;
+
+    await subject.save();
+
+    return { status: 200, error: undefined, data: title };
   }
 
- 
- 
- 
- 
- 
   async update({ request }) {
     const { body, params } = request;
 
@@ -88,40 +72,21 @@ class SubjectController {
 
     return { status: 200, error: undefined, data: subject };
   }
- 
- 
- 
- 
- 
- 
- 
- 
+
   async destroy({ request }) {
     const { id } = request.params;
 
-    await Database
-    .table("subjects")
-    .where({ subject_id: id })
-    .delete();
+    await Database.table("subjects").where({ subject_id: id }).delete();
 
     return { status: 200, error: undefined, data: { massage: "success" } };
   }
 
-  
-  
-  
-  
-  
-  
-  
-  async showTeacher({request}){
-    const {id} =request.params
-    const subject =await Database
-    .table('subjects')
-    .where({subject_id: id})
-    .innerJoin('teachers','subjects.teacher_id','teachers.teacher_id')
-    .first();
-
+  async showTeacher({ request }) {
+    const { id } = request.params;
+    const subject = await Database.table("subjects")
+      .where({ subject_id: id })
+      .innerJoin("teachers", "subjects.teacher_id", "teachers.teacher_id")
+      .first();
   }
 }
 
