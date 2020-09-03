60974a14c9d0c6697c03d2fe389715d5f0547e9b
walax31 <walax31009@gmail.com>
2020-09-03 20:08:22 +0700

    edit format

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -3,7 +3,7 @@
 const Database = use("Database");
 const Hash = use("Hash");
 const Validator = use("Validator");
-const Student =use('App/Models/Student')
+const Student = use("App/Models/Student");
 
 function numberTypeParamValidator(number) {
   if (Number.isNaN(parseInt(number))) {
@@ -14,34 +14,30 @@ function numberTypeParamValidator(number) {
   return {};
 }
 class StudentController {
-  async index({request}) {
-    const {references=undefined} =request.qs
-    const students =Student.query()
-    if (references){
-      const extractedReferences =references.split(",")
-       students.with(extractedReferences)
+  async index({ request }) {
+    const { references = undefined } = request.qs;
+    const students = Student.query();
+    if (references) {
+      const extractedReferences = references.split(",");
+      students.with(extractedReferences);
     }
 
-    return { status: 200, error: undefined, data:await students.fetch()  };
+    return { status: 200, error: undefined, data: await students.fetch() };
   }
-
-
 
   async show({ request }) {
     const { id } = request.params;
-    const student =await Student.find(id)
+    const student = await Student.find(id);
     const validateValue = numberTypeParamValidator(id);
 
     if (validateValue.error)
-        return { status: 500, error: validateValue.error, data: undefined };
-
+      return { status: 500, error: validateValue.error, data: undefined };
 
     return { status: 200, error: undefined, data: students || {} };
   }
-  
-  
+
   async store({ request }) {
-    const { first_name, last_name, email, password,group_id } = request.body;
+    const { first_name, last_name, email, password, group_id } = request.body;
 
     const rules = {
       first_name: "required",
@@ -55,12 +51,12 @@ class StudentController {
       return { status: 422, error: validation.messages(), data: undefined };
 
     const hashedPassword = await Hash.make(password);
-    const student =new Student()
-    student.first_name=first_name
-    student.last_name=last_name
-    student.email=email
-    student.password=password
-    await student.save()
+    const student = new Student();
+    student.first_name = first_name;
+    student.last_name = last_name;
+    student.email = email;
+    student.password = password;
+    await student.save();
 
     return {
       status: 200,
